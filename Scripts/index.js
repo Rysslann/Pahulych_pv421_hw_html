96ccb66da736ce17e1a2920de09c400fc04567b9
Rysslann <ryslan10836@gmail.com>
2024-04-19 23:00:20 +0300

    v0.1

--- Scripts/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    BURGER_BTN = document.querySelector("#burger");
-    BURGER_BTN.addEventListener("click", () => {
-      BURGER_BTN.classList.toggle("active");
-    });
-  });
